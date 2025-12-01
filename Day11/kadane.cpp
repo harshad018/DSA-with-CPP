2e40bb286d12a7c4e8a4c186ce7517dfa70863fb
Harshad Ghutukade <108193335+harshad018@users.noreply.github.com>
2025-12-01 19:04:17 +0530

    Correct current sum logic in maxSubarraySum function
    Fix current sum initialization and update logic for max subarray sum calculation.

--- Day11/kadane.cpp
+++ Day11/kadane.cpp
@@ -7,14 +7,14 @@ using namespace std;
 void maxSubarraySum(int arr[], int n){
 
     int maxSum = INT_MIN;
-    int currSum = 1;
+    int currSum = 0;
     for ( int start = 0; start < n; start++){
         
-       currSum *= arr[start];
+       currSum += arr[start];
        maxSum = max( maxSum, currSum);
 
        if ( currSum < 0){
-            currSum = 1;
+            currSum = 0;
        }
         
     }
